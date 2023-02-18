5d1b80cbf6650194f20314375d95f44d5de23578
Felix12390 <felixaa@uia.no>
2023-02-18 18:40:17 +0100

    prøvde på funfacts

--- funfacts/funfacts_test.go
+++ funfacts/funfacts_test.go
@@ -13,14 +13,19 @@ import (
 */
 func TestGetFunFacts(t *testing.T) {
 	type test struct {
-		input // her må du skrive riktig type for input
-		want  // her må du skrive riktig type for returverdien
+		input string // her må du skrive riktig type for input
+		want  string // her må du skrive riktig type for returverdien
 	}
 
 	// Her må du legge inn korrekte testverdier
 	//tests := []test{
 	//  {input: , want: },
 	//}
+	tests := []test{
+		{input: "sun", want: "sola"},
+		{input: "luna", want: "månen"},
+		{input: "terra", want: "jorda"},
+	}
 
 	for _, tc := range tests {
 		got := GetFunFacts(tc.input)
